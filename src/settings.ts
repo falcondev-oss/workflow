733ae793db7b4881674744c146701630e8506da2
Louis Haftmann <30736553+LouisHaftmann@users.noreply.github.com>
2026-01-26 09:24:20 +0100

    feat: redis connection helper

--- src/settings.ts
+++ src/settings.ts
@@ -1,3 +1,4 @@
+import type { RedisOptions } from 'ioredis'
 import { createSingletonPromise } from '@antfu/utils'
 import IORedis from 'ioredis'
 
@@ -8,15 +9,31 @@ export type WorkflowLogger = {
 
 export const Settings = {
   defaultPrefix: 'falcondev-oss-workflow',
-  defaultConnection: undefined as IORedis | undefined,
+  defaultConnection: undefined as (() => Promise<IORedis> | IORedis) | undefined,
   defaultCronTimezone: undefined as string | undefined,
   logger: undefined as WorkflowLogger | undefined,
 }
 
-export const defaultRedisConnection = createSingletonPromise(async () => {
-  if (Settings.defaultConnection) return Settings.defaultConnection
+const defaultRedisOptions: RedisOptions = {
+  lazyConnect: true,
+  maxRetriesPerRequest: null,
+  retryStrategy: (times) => Math.max(Math.min(Math.exp(times), 20_000), 1000),
+  enableOfflineQueue: false,
+}
 
-  const redis = new IORedis({ lazyConnect: true, maxRetriesPerRequest: null })
+export const defaultRedisConnection = createSingletonPromise(async () => {
+  if (Settings.defaultConnection) return Settings.defaultConnection()
+
+  const redis = new IORedis(defaultRedisOptions)
   await redis.connect()
   return redis
 })
+
+export async function createRedisConnection(opts: RedisOptions) {
+  const redis = new IORedis({
+    ...defaultRedisOptions,
+    ...opts,
+  })
+  await redis.connect()
+  return redis
+}
